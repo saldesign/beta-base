ef07c160fb8d0ccfabafd627d16639bb5dc8ddb5
saldesign <saldarriagadesign@gmail.com>
2016-03-16 11:26:53 -0700

    begin html

---
+++ beta-base/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home - Beta-Base</title>
+	<link rel="stylesheet" type="text/css" href="reset.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<header>
+			<h1><a href="#">Beta-Base<span>Rock Climbing Guide</span></a></h1>
+			<nav class="main-nav" role="navigation">
+				<ul>
+					<li><a href="#">Sign In/Up</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Share</a></li>
+					<li><a href="#">Find</a></li>
+				</ul>
+			</nav>
+	</header>
+
+	<main>
+
+		<section class="banner">
+			<h2>Where climbers come to <a href="#">share</a> and <a href="#">find</a> rock climbing beta</h2>
+		</section>
+
+		<div class="quicklinks">
+			<section class="module">
+				<h3>Share</h3>
+				<p>Share developed climbing routes along with descriptions, gradings, ratings, coordinates and images</p>
+				<a href="#" class="btn">Get Started</a>
+			</section>
+			<section class="module">
+				<h3>Find</h3>
+				<p>Share developed climbing routes along with descriptions, gradings, ratings, coordinates and images</p>
+				<a href="#" class="btn">Get Started</a>
+			</section>
+			<section class="module">
+				<h3>Join</h3>
+				<p>Share developed climbing routes along with descriptions, gradings, ratings, coordinates and images</p>
+				<a href="#" class="btn">Get Started</a>
+			</section>
+		</div>
+
+		<section class="module secondary">
+			<h4>Recent Shares</h4>
+			<article>
+				<h5>CLIMB NAME<span>GRADE</span></h5>
+				<h6>AREA<span class="rating">RATING</span></h6>
+				<p>AUTHOR<span>DATE</span></p>
+				<p>A clean, short, 3 move, hand crack on the left most behive boulder</p>
+				<a href="#">Read More...</a>
+			</article>
+			<article>
+				<h5>CLIMB NAME<span>GRADE</span></h5>
+				<h6>AREA<span class="rating">RATING</span></h6>
+				<p>AUTHOR<span>DATE</span></p>
+				<p>A clean, short, 3 move, hand crack on the left most behive boulder</p>
+				<a href="#">Read More...</a>
+			</article>
+			<article>
+				<h5>CLIMB NAME<span>GRADE</span></h5>
+				<h6>AREA<span class="rating">RATING</span></h6>
+				<p>AUTHOR<span>DATE</span></p>
+				<p>A clean, short, 3 move, hand crack on the left most behive boulder</p>
+				<a href="#">Read More...</a>
+			</article>
+		</section>
+
+		<section class="module secondary">
+			<h4>Recent Climbs</h4>
+			<article>
+				<h5>CLIMB NAME<span>GRADE</span></h5>
+				<h6>AREA<span class="rating">RATING</span></h6>
+				<p>AUTHOR<span>TICKS</span></p>
+			</article>
+			<article>
+				<h5>CLIMB NAME<span>GRADE</span></h5>
+				<h6>AREA<span class="rating">RATING</span></h6>
+				<p>AUTHOR<span>TICKS</span></p>
+			</article>
+			<article>
+				<h5>CLIMB NAME<span>GRADE</span></h5>
+				<h6>AREA<span class="rating">RATING</span></h6>
+				<p>AUTHOR<span>TICKS</span></p>
+			</article>
+		</section>
+	<main>
+
+	<footer>
+		<h2><a href="#">Beta-Base Rock Climbing Guide</a></h2>
+		<a href="#" class="btn">SIGN IN or SIGN UP</a>
+		<h4>CONTACT US</h4>
+		<a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">Send Mail</a>
+	</footer>
+
+</body>
+</html>
