169814407dd107e1db7437014104eb6e3fbdc35a
saldesign <saldarriagadesign@gmail.com>
2016-03-21 11:57:21 -0700

    maps api

---
+++ google_api/map.js
@@ -0,0 +1,27 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 32.7150, lng: -117.1625},
+    zoom: 10,
+    styles: [{
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+    }, {
+      featureType: 'transit.station',
+      stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+    }],
+    disableDoubleClickZoom: true
+  });
+
+  map.addListener('click', function(e) {
+    var marker = new google.maps.Marker({
+      position: {lat: e.latLng.lat(), lng: e.latLng.lng()},
+      map: map,
+      draggable: true
+    });
+
+  });
+}
+
+
+//http://jsfiddle.net/kjy112/QvNUF/
+//http://stackoverflow.com/questions/5290336/getting-lat-lng-from-google-marker
